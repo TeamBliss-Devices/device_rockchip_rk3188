ab27de2017e7f5600b7af50be2e585d5ac28c622
erwan Leboucher <erwanleboucher@gmail.com>
2015-04-14 12:50:21 +0200

    Update bliss.mk

--- bliss.mk
+++ bliss.mk
@@ -3,7 +3,7 @@ include device/rockchip/rk3188/BoardConfig.mk
 $(call inherit-product, device/rockchip/rksdk/device.mk)
 PRODUCT_BRAND := rockchip
 PRODUCT_DEVICE := rk3188
-PRODUCT_NAME := rk3188
+PRODUCT_NAME := bliss_rk3188
 PRODUCT_MANUFACTURER := Newsmy
 PRODUCT_MODEL := CarPad-II-P
 
